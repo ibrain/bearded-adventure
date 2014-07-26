993fef63f625a85a1129bbdd85f4f2bc09fdb706
ibrain <meowmaster@yandex.ua>
2014-07-26 11:45:19 +0400

    Fixed mysql to the open wrapper
    Changed function to the open wrapper to comfort

--- controllers/mysql.php
+++ controllers/mysql.php
@@ -1,21 +1,22 @@
 <?php 
 	
-	@require_once("config.php");
 
+	$mysql_host = "localhost";
+	$mysql_username = "root";
+	$mysql_password = "";
+	$mysql_dbname = "bearded";
 
-	function core_mysql_connect()
-	{
-		$connection = mysql_connect($mysql_host,$mysql_login,$mysql_password);
+		$connection = mysql_connect($mysql_host,$mysql_username,$mysql_password);
 		mysql_select_db($mysql_dbname);
-		mysql_query("SET names utf8;");
-		return $connection;
-	}
+		mysql_query("SET names 'utf8';");
+
 
 	function core_mysql_query($query_string)
 	{
 		$result = mysql_query($query_string);
 	}
 	
+
 
 
  ?>
